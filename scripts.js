b076556aa086f056e94aaf0c64cc88206a2b2b4c
HarryStevens <harryjosephstevens@gmail.com>
2014-02-25 10:45:34 -0500

    some more javascript stuff

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/**
+ * @author Harry
+ */
+
+/* 
+ * Script Outline:
+ * 1. Set up $(document).ready() with a function to load when everything is loaded.
+ * 2. Load Google Viz library.
+ * 3. Load data.
+ * 4. Feed data to library to place result on page.
+ */
+$(document).ready(pageLoaded);
+function pageLoaded(){
+	console.log('page loaded');
+}
