564c6aa2ea85083597d163d5f852c796e12efa05
HarryStevens <harryjosephstevens@gmail.com>
2014-02-25 12:36:56 -0500

    messing with axes

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@
 function dataLoaded(UNEMPDATA){
 	//(4)(a)
 	var myObsData = UNEMPDATA.observations;
-	var myHeaders = ["Date","Civilians unemployed for 27 weeks or more"];
+	var myHeaders = ["Date","Number of long-term unemployed"];
 	var myDataArray = [];
 	myDataArray.push(myHeaders);
 	//(4)(b)
@@ -35,15 +35,59 @@ function dataLoaded(UNEMPDATA){
 	var data = google.visualization.arrayToDataTable(myDataArray);
 	var options = {
 		title: 'Long-term Unemployment 1980-present',
+		hAxis: {title:'Date', ticks:[
+			new Date(1980,1,1),
+			new Date(1981,1,1),
+			new Date(1982,1,1),
+			new Date(1983,1,1),
+			new Date(1984,1,1),
+			new Date(1985,1,1),
+			new Date(1986,1,1),
+			new Date(1987,1,1),
+			new Date(1988,1,1),
+			new Date(1989,1,1),
+			new Date(1990,1,1),
+			new Date(1991,1,1),
+			new Date(1992,1,1),
+			new Date(1993,1,1),
+			new Date(1994,1,1),
+			new Date(1995,1,1),
+			new Date(1996,1,1),
+			new Date(1997,1,1),
+			new Date(1998,1,1),
+			new Date(1999,1,1),
+			new Date(2000,1,1),
+			new Date(2001,1,1),
+			new Date(2002,1,1),
+			new Date(2003,1,1),
+			new Date(2004,1,1),
+			new Date(2005,1,1),
+			new Date(2006,1,1),
+			new Date(2007,1,1),
+			new Date(2008,1,1),
+			new Date(2009,1,1),
+			new Date(2010,1,1),
+			new Date(2011,1,1),
+			new Date(2012,1,1),
+			new Date(2013,1,1)
+		]},
+		vAxis: {title:'Civilians unemployed for 27 weeks or more', ticks:[
+			1000000,
+			2000000,
+			3000000,
+			4000000,
+			5000000,
+			6000000,
+			7000000,
+			8000000]},
 		curveType: 'function',
-		legend: { position: 'bottom' },
 		height: 550,
-		width: 1400
-		};
+		width: 1400,
+		
+	};
 	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 	chart.draw(data, options);
 
-	
 }//end dataLoaded
 
 
